4fe290b70f5bee53788c273f0395e9e88e36cf1c
Noah Dumangin <noah.dumangin@etu.unistra.fr>
2025-06-10 14:24:22 +0200

    volet 5 sans les usercontrol

--- prjPompiers/Volets5.Designer.cs
+++ prjPompiers/Volets5.Designer.cs
@@ -28,12 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpTitre = new System.Windows.Forms.GroupBox();
+            this.pbgif2 = new System.Windows.Forms.PictureBox();
+            this.pbgif = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grpStat1caserne = new System.Windows.Forms.GroupBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEngin = new System.Windows.Forms.Label();
+            this.lblPlusutilise = new System.Windows.Forms.Label();
+            this.cbo1caserne = new System.Windows.Forms.ComboBox();
+            this.lbl1caserne = new System.Windows.Forms.Label();
+            this.grpEnsembleCaserne = new System.Windows.Forms.GroupBox();
+            this.lblAffichage = new System.Windows.Forms.Label();
+            this.cboPartie2 = new System.Windows.Forms.ComboBox();
+            this.btnfermer = new System.Windows.Forms.Button();
+            this.grpTitre.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbgif2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbgif)).BeginInit();
+            this.grpStat1caserne.SuspendLayout();
+            this.grpEnsembleCaserne.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpTitre
+            // 
+            this.grpTitre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.grpTitre.Controls.Add(this.pbgif2);
+            this.grpTitre.Controls.Add(this.pbgif);
+            this.grpTitre.Controls.Add(this.label2);
+            this.grpTitre.ForeColor = System.Drawing.Color.White;
+            this.grpTitre.Location = new System.Drawing.Point(12, 13);
+            this.grpTitre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpTitre.Name = "grpTitre";
+            this.grpTitre.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpTitre.Size = new System.Drawing.Size(1983, 161);
+            this.grpTitre.TabIndex = 5;
+            this.grpTitre.TabStop = false;
+            // 
+            // pbgif2
+            // 
+            this.pbgif2.ErrorImage = null;
+            this.pbgif2.ImageLocation = "";
+            this.pbgif2.Location = new System.Drawing.Point(1701, 23);
+            this.pbgif2.Margin = new System.Windows.Forms.Padding(4);
+            this.pbgif2.Name = "pbgif2";
+            this.pbgif2.Size = new System.Drawing.Size(253, 116);
+            this.pbgif2.TabIndex = 3;
+            this.pbgif2.TabStop = false;
+            // 
+            // pbgif
+            // 
+            this.pbgif.ErrorImage = null;
+            this.pbgif.ImageLocation = "";
+            this.pbgif.Location = new System.Drawing.Point(44, 23);
+            this.pbgif.Margin = new System.Windows.Forms.Padding(4);
+            this.pbgif.Name = "pbgif";
+            this.pbgif.Size = new System.Drawing.Size(253, 116);
+            this.pbgif.TabIndex = 2;
+            this.pbgif.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(887, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "STATISTIQUES";
+            // 
+            // grpStat1caserne
+            // 
+            this.grpStat1caserne.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.grpStat1caserne.Controls.Add(this.lblNombre);
+            this.grpStat1caserne.Controls.Add(this.lblEngin);
+            this.grpStat1caserne.Controls.Add(this.lblPlusutilise);
+            this.grpStat1caserne.Controls.Add(this.cbo1caserne);
+            this.grpStat1caserne.Controls.Add(this.lbl1caserne);
+            this.grpStat1caserne.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.grpStat1caserne.ForeColor = System.Drawing.Color.White;
+            this.grpStat1caserne.Location = new System.Drawing.Point(12, 203);
+            this.grpStat1caserne.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpStat1caserne.Name = "grpStat1caserne";
+            this.grpStat1caserne.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpStat1caserne.Size = new System.Drawing.Size(1983, 385);
+            this.grpStat1caserne.TabIndex = 6;
+            this.grpStat1caserne.TabStop = false;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.lblNombre.Location = new System.Drawing.Point(1003, 146);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(0, 20);
+            this.lblNombre.TabIndex = 6;
+            // 
+            // lblEngin
+            // 
+            this.lblEngin.AutoSize = true;
+            this.lblEngin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.lblEngin.Location = new System.Drawing.Point(1775, 84);
+            this.lblEngin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEngin.Name = "lblEngin";
+            this.lblEngin.Size = new System.Drawing.Size(0, 20);
+            this.lblEngin.TabIndex = 5;
+            // 
+            // lblPlusutilise
+            // 
+            this.lblPlusutilise.AutoSize = true;
+            this.lblPlusutilise.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblPlusutilise.Location = new System.Drawing.Point(1291, 122);
+            this.lblPlusutilise.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPlusutilise.Name = "lblPlusutilise";
+            this.lblPlusutilise.Size = new System.Drawing.Size(253, 29);
+            this.lblPlusutilise.TabIndex = 4;
+            this.lblPlusutilise.Text = "Engin le plus utilisé : ";
+            // 
+            // cbo1caserne
+            // 
+            this.cbo1caserne.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.cbo1caserne.FormattingEnabled = true;
+            this.cbo1caserne.Location = new System.Drawing.Point(349, 36);
+            this.cbo1caserne.Margin = new System.Windows.Forms.Padding(4);
+            this.cbo1caserne.Name = "cbo1caserne";
+            this.cbo1caserne.Size = new System.Drawing.Size(325, 37);
+            this.cbo1caserne.TabIndex = 2;
+            this.cbo1caserne.SelectionChangeCommitted += new System.EventHandler(this.cbo1caserne_SelectionChangeCommitted);
+            // 
+            // lbl1caserne
+            // 
+            this.lbl1caserne.AutoSize = true;
+            this.lbl1caserne.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lbl1caserne.Location = new System.Drawing.Point(32, 39);
+            this.lbl1caserne.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1caserne.Name = "lbl1caserne";
+            this.lbl1caserne.Size = new System.Drawing.Size(297, 29);
+            this.lbl1caserne.TabIndex = 0;
+            this.lbl1caserne.Text = "Choisissez une caserne :";
+            // 
+            // grpEnsembleCaserne
+            // 
+            this.grpEnsembleCaserne.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.grpEnsembleCaserne.Controls.Add(this.lblAffichage);
+            this.grpEnsembleCaserne.Controls.Add(this.cboPartie2);
+            this.grpEnsembleCaserne.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.grpEnsembleCaserne.ForeColor = System.Drawing.Color.White;
+            this.grpEnsembleCaserne.Location = new System.Drawing.Point(12, 622);
+            this.grpEnsembleCaserne.Margin = new System.Windows.Forms.Padding(4);
+            this.grpEnsembleCaserne.Name = "grpEnsembleCaserne";
+            this.grpEnsembleCaserne.Padding = new System.Windows.Forms.Padding(4);
+            this.grpEnsembleCaserne.Size = new System.Drawing.Size(1983, 389);
+            this.grpEnsembleCaserne.TabIndex = 7;
+            this.grpEnsembleCaserne.TabStop = false;
+            // 
+            // lblAffichage
+            // 
+            this.lblAffichage.AutoSize = true;
+            this.lblAffichage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblAffichage.Location = new System.Drawing.Point(13, 38);
+            this.lblAffichage.Name = "lblAffichage";
+            this.lblAffichage.Size = new System.Drawing.Size(466, 29);
+            this.lblAffichage.TabIndex = 2;
+            this.lblAffichage.Text = "Choisissez ce que vous voulez afficher :";
+            // 
+            // cboPartie2
+            // 
+            this.cboPartie2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.cboPartie2.FormattingEnabled = true;
+            this.cboPartie2.Location = new System.Drawing.Point(499, 34);
+            this.cboPartie2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboPartie2.Name = "cboPartie2";
+            this.cboPartie2.Size = new System.Drawing.Size(433, 37);
+            this.cboPartie2.TabIndex = 1;
+            this.cboPartie2.SelectionChangeCommitted += new System.EventHandler(this.cboPartie2_SelectionChangeCommitted);
+            // 
+            // btnfermer
+            // 
+            this.btnfermer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.btnfermer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnfermer.ForeColor = System.Drawing.Color.White;
+            this.btnfermer.Location = new System.Drawing.Point(12, 1034);
+            this.btnfermer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnfermer.Name = "btnfermer";
+            this.btnfermer.Size = new System.Drawing.Size(340, 39);
+            this.btnfermer.TabIndex = 8;
+            this.btnfermer.Text = "Fermer les statistiques";
+            this.btnfermer.UseVisualStyleBackColor = false;
+            this.btnfermer.Click += new System.EventHandler(this.btnfermer_Click);
+            // 
+            // Volets5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1924, 1175);
+            this.Controls.Add(this.grpTitre);
+            this.Controls.Add(this.grpStat1caserne);
+            this.Controls.Add(this.grpEnsembleCaserne);
+            this.Controls.Add(this.btnfermer);
+            this.Name = "Volets5";
             this.Text = "Volets5";
+            this.Load += new System.EventHandler(this.Volets5_Load);
+            this.grpTitre.ResumeLayout(false);
+            this.grpTitre.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbgif2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbgif)).EndInit();
+            this.grpStat1caserne.ResumeLayout(false);
+            this.grpStat1caserne.PerformLayout();
+            this.grpEnsembleCaserne.ResumeLayout(false);
+            this.grpEnsembleCaserne.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpTitre;
+        private System.Windows.Forms.PictureBox pbgif2;
+        private System.Windows.Forms.PictureBox pbgif;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grpStat1caserne;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEngin;
+        private System.Windows.Forms.Label lblPlusutilise;
+        private System.Windows.Forms.ComboBox cbo1caserne;
+        private System.Windows.Forms.Label lbl1caserne;
+        private System.Windows.Forms.GroupBox grpEnsembleCaserne;
+        private System.Windows.Forms.Label lblAffichage;
+        private System.Windows.Forms.ComboBox cboPartie2;
+        private System.Windows.Forms.Button btnfermer;
     }
 }
--- prjPompiers/Volets5.cs
+++ prjPompiers/Volets5.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,289 @@ using System.Windows.Forms;
 
 namespace prjPompiers
 {
+    //affichage des usercontrols
+    //affichagevolet5 uc = new affichagevolet5();
+    //affichagevolet5 uc1 = new affichagevolet5();
     public partial class Volets5 : Form
     {
         public Volets5()
         {
             InitializeComponent();
+        }
+
+        private void Volets5_Load(object sender, EventArgs e)
+        {
+            pbgif.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbgif2.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.WindowState = FormWindowState.Maximized;
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+            lblPlusutilise.Visible = false;
+
+            //usercontrols
+            /*uc.Top = 80;
+            uc.Left = 20;
+            uc1.Top = 80;
+            uc1.Left = 20;
+            uc1.Height = 1000;
+            grpStat1caserne.Controls.Add(uc);
+            grpEnsembleCaserne.Controls.Add(uc1);*/
+
+
+            //charger la cbo pour choisir la caserne
+            string req = "SELECT * FROM Caserne";
+            SQLiteDataAdapter da = new SQLiteDataAdapter(req, Connexion.Connec);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cbo1caserne.DataSource = dt;
+            cbo1caserne.DisplayMember = "nom";
+            cbo1caserne.ValueMember = "id";
+            cbo1caserne.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            //charger la cbo pour l'ensemble des casernes
+
+            Dictionary<int, string> choix = new Dictionary<int, string>
+            {
+                { 1, "Nombre d'interventions par sinistre" },
+                { 2, "Habilitations les plus sollicitées" },
+                { 3, "Pompier par habilitation" }
+            };
+
+            cboPartie2.DataSource = new BindingSource(choix, null);
+            cboPartie2.DisplayMember = "Value";
+            cboPartie2.ValueMember = "Key";
+            cboPartie2.DropDownStyle = ComboBoxStyle.DropDownList;
+
+        }
+
+        private void cbo1caserne_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            DataSet ds = MesDatas.DsGlobal;
+            int caserne = Convert.ToInt32(cbo1caserne.SelectedValue);
+            try
+            {
+                string reqdgv = @"SELECT e.numero AS 'N° d''engin', t.nom AS 'Nom de l''engin', t.code AS 'Code d''engin', COUNT(pa.idMission) AS 'Nombre d''utilisations', ROUND(SUM((JULIANDAY(m.dateHeureRetour) - JULIANDAY(m.dateHeureDepart)) * 24), 2) AS 'utilisation (en heures)'
+                                          FROM Engin e
+                                          LEFT JOIN PartirAvec pa ON pa.numeroEngin = e.numero AND pa.codeTypeEngin = e.codeTypeEngin
+                                          LEFT JOIN Mission m ON m.id = pa.idMission
+                                          JOIN TypeEngin t ON e.codeTypeEngin = t.code
+                                          WHERE e.idCaserne = " +
+                                          caserne +
+                                        @" GROUP BY e.numero, t.nom, t.code
+                                          ORDER BY ""utilisation (en heures)"" DESC, COUNT(pa.idMission) DESC, e.numero;";
+                SQLiteCommand cmd = new SQLiteCommand(reqdgv, Connexion.Connec);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                //uc.majdonnee(dt);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+            try
+            {
+                lblPlusutilise.Visible = true;
+                lblEngin.Location = new Point(lblPlusutilise.Location.X + lblPlusutilise.Width + 2, lblPlusutilise.Location.Y);
+                lblEngin.Font = new Font(lblEngin.Font.FontFamily, 15);
+
+
+                //afficher l'engin le plus utilisé
+                string reqPlusUtilise = @"SELECT t.nom
+                                            FROM Engin e
+                                            JOIN TypeEngin t ON e.codeTypeEngin = t.code
+                                            JOIN PartirAvec pa ON pa.numeroEngin = e.numero AND pa.codeTypeEngin = e.codeTypeEngin
+                                            WHERE e.idCaserne = @idCaserne
+                                            GROUP BY e.numero, t.nom
+                                            ORDER BY COUNT(pa.idMission) DESC
+                                            LIMIT 1;";
+
+                SQLiteCommand cmdPlusUtilise = new SQLiteCommand(reqPlusUtilise, Connexion.Connec);
+                cmdPlusUtilise.Parameters.AddWithValue("@idCaserne", caserne);
+
+                object result = cmdPlusUtilise.ExecuteScalar();
+                if (result != null)
+                {
+                    lblEngin.Text = result.ToString();
+                }
+                else
+                {
+                    lblEngin.Text = "Aucun engin utilisé";
+                }
+
+                //afficher le nombre d'utilisations
+
+                string reqNbUtilisations = @"SELECT COUNT(pa.idMission)
+                                            FROM Engin e
+                                            JOIN PartirAvec pa ON pa.numeroEngin = e.numero AND pa.codeTypeEngin = e.codeTypeEngin
+                                            JOIN TypeEngin t ON t.code = e.codeTypeEngin
+                                            WHERE e.idCaserne = @idCaserne
+                                            GROUP BY e.numero, t.nom, t.code
+                                            ORDER BY COUNT(pa.idMission) DESC
+                                            LIMIT 1;";
+
+                SQLiteCommand cmdNbUtilisations = new SQLiteCommand(reqNbUtilisations, Connexion.Connec);
+                cmdNbUtilisations.Parameters.AddWithValue("@idCaserne", caserne);
+                object resultUtilisation = cmdNbUtilisations.ExecuteScalar();
+                if (resultUtilisation != null)
+                {
+                    lblNombre.Text = "Nombre d'utilisations :   " + resultUtilisation.ToString();
+                }
+                else
+                {
+                    lblNombre.Text = "Nombre d'utilisations :   0";
+                }
+                lblNombre.Location = new Point(lblPlusutilise.Location.X, lblPlusutilise.Location.Y + lblPlusutilise.Height + 20);
+                lblNombre.Font = new Font(lblNombre.Font.FontFamily, 15);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void cboPartie2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet ds = MesDatas.DsGlobal;
+                int choix = (int)((KeyValuePair<int, string>)cboPartie2.SelectedItem).Key;
+
+                AfficherDansListView(choix);
+            }
+            catch (Exception erreur)
+            {
+                MessageBox.Show(erreur.Message);
+            }
+        }
+        private void AfficherDansListView(int choix)
+        {
+            try
+            {
+                if (choix == 1)
+                {
+                    string reqdgv = @"SELECT s.libelle AS ""Type de sinistre"", COUNT(m.id) AS ""Nombre d'interventions""
+                                    FROM NatureSinistre s
+                                    LEFT JOIN Mission m ON m.idNatureSinistre = s.id
+                                    GROUP BY s.libelle
+                                    ORDER BY COUNT(m.id) DESC;";
+                    SQLiteCommand cmd = new SQLiteCommand(reqdgv, Connexion.Connec);
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    //uc1.majdonnee(dt);
+                }
+                else if (choix == 2)
+                {
+                    string reqdgv = @"SELECT h.libelle, COUNT(m.id) AS ""nombre de sollicitations""
+                                    FROM Habilitation h
+                                    LEFT JOIN Embarquer e ON h.id = e.idHabilitation
+                                    LEFT JOIN Necessiter n ON e.codeTypeEngin = n.codeTypeEngin
+                                    LEFT JOIN Mission m ON n.idNatureSinistre = m.idNatureSinistre
+                                    GROUP BY h.id
+                                    ORDER BY COUNT(m.id) DESC;";
+                    SQLiteCommand cmd = new SQLiteCommand(reqdgv, Connexion.Connec);
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    //uc1.majdonnee(dt);
+                }
+                else if (choix == 3)
+                {
+
+                    // création du label
+                    Label lblHabilitations = new Label();
+                    lblHabilitations.Name = "lblHabilitations";
+                    lblHabilitations.Text = "Choisissez l'habilitation :";
+                    lblHabilitations.Font = new Font("Microsoft Sans Serif", 15F);
+                    lblHabilitations.AutoSize = true;
+                    lblHabilitations.Location = new Point(296 + cboPartie2.Width + 80, 35);
+                    grpEnsembleCaserne.Controls.Add(lblHabilitations);
+
+                    // création de la combobox
+                    ComboBox cboHabilitations = new ComboBox();
+                    cboHabilitations.Name = "cboHabilitations";
+                    cboHabilitations.DropDownStyle = ComboBoxStyle.DropDownList;
+                    cboHabilitations.Font = new Font("Microsoft Sans Serif", 15F);
+                    cboHabilitations.Location = new Point(lblHabilitations.Right + 10, 35);
+                    cboHabilitations.DropDownStyle = ComboBoxStyle.DropDownList;
+                    grpEnsembleCaserne.Controls.Add(cboHabilitations);
+
+
+                    //remplissage de la combobox
+                    string reqHabilitations = "SELECT * FROM Habilitation";
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(reqHabilitations, Connexion.Connec);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    cboHabilitations.DataSource = dt;
+                    cboHabilitations.DisplayMember = "libelle";
+                    cboHabilitations.ValueMember = "id";
+
+                    //ajuste la taille de la combobox
+                    int maxWidth = 0;
+                    using (Graphics g = cboHabilitations.CreateGraphics())
+                    {
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            string text = row["libelle"].ToString();
+                            int width = TextRenderer.MeasureText(text, cboHabilitations.Font).Width;
+                            if (width > maxWidth) maxWidth = width;
+                        }
+                    }
+                    cboHabilitations.Width = maxWidth + 30;
+
+                    cboHabilitations.SelectionChangeCommitted += cboHabilitations_SelectionChangeCommitted;
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void cboHabilitations_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            // afficher dans le DataGridView
+            ComboBox cbo = (ComboBox)sender;
+            int habilitation = Convert.ToInt32(cbo.SelectedValue);
+
+            string reqdgv = @"SELECT h.libelle AS Habilitation, p.nom || ' ' || p.prenom AS Pompier
+                                    FROM Habilitation h
+                                    LEFT JOIN Passer a ON a.idHabilitation = h.id
+                                    LEFT JOIN Pompier p ON p.matricule = a.matriculePompier
+                                    WHERE h.id = " + habilitation + @" 
+                                    ORDER BY h.libelle, p.nom;";
+
+            SQLiteCommand cmd = new SQLiteCommand(reqdgv, Connexion.Connec);
+            SQLiteDataAdapter dadapt = new SQLiteDataAdapter(cmd);
+
+            DataTable dtable = new DataTable();
+            dadapt.Fill(dtable);
+            //uc1.majdonnee(dtable);
+            // Vérifie si aucun pompier
+            bool aucunPompier = true;
+            foreach (DataRow row in dtable.Rows)
+            {
+                if (!string.IsNullOrEmpty(row["Pompier"]?.ToString()))
+                {
+                    aucunPompier = false;
+                    break;
+                }
+            }
+
+            if (dtable.Rows.Count == 0 || aucunPompier)
+            {
+                MessageBox.Show("Aucun pompier trouvé pour cette habilitation.");
+            }
+        }
+
+        private void btnfermer_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
