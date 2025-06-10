adf0da41f44f567e589df1ffae8f93d5da73f24b
Noah Dumangin <noah.dumangin@etu.unistra.fr>
2025-06-10 15:03:04 +0200

    ajout uc volet 5

---
+++ ucaffichagevolet5/affichagevolet5/affichagevolet5.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace affichagevolet5
+{
+    partial class affichagevolet5
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lv
+            // 
+            this.lv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.lv.HideSelection = false;
+            this.lv.Location = new System.Drawing.Point(0, 0);
+            this.lv.Name = "lv";
+            this.lv.Size = new System.Drawing.Size(932, 211);
+            this.lv.TabIndex = 0;
+            this.lv.UseCompatibleStateImageBehavior = false;
+            // 
+            // affichagevolet5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.Controls.Add(this.lv);
+            this.Name = "affichagevolet5";
+            this.Size = new System.Drawing.Size(932, 211);
+            this.Load += new System.EventHandler(this.affichagevolet5_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv;
+    }
+}
